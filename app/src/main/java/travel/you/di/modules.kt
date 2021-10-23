37ba00f05a68e50947d650e365bd38779a111ade
1_skat_1 <skat.apps@gmail.com>
2021-10-24 02:08:17 +0300

    implementations createNewTrip and add VariantsFragment.kt

--- app/src/main/java/travel/you/di/modules.kt
+++ app/src/main/java/travel/you/di/modules.kt
@@ -2,10 +2,9 @@ package travel.you.di
 
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import travel.you.trip.CreateNewTripViewModel
 
 val modules = module {
-
-
-
+    viewModel { CreateNewTripViewModel() }
 
 }
