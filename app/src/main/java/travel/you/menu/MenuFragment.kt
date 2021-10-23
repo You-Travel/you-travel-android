0a872b071672d1a76b98445dbf09b3e39cb7d582
Nikita Koschin <nik.koshin@mail.ru>
2021-10-24 00:27:33 +0300

    add car pic

--- app/src/main/java/travel/you/menu/MenuFragment.kt
+++ app/src/main/java/travel/you/menu/MenuFragment.kt
@@ -1,9 +1,16 @@
 package travel.you.menu
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import travel.you.R
+import travel.you.databinding.CardMenuBinding
 
 import travel.you.databinding.FragmentMenuBinding
 
@@ -20,6 +27,28 @@ class MenuFragment : Fragment() {
     ): View? {
         binding = FragmentMenuBinding.inflate(inflater)
         viewModel = ViewModelProviders.of(this).get(MenuViewModel::class.java)
+
+        var plases = listOf<String>("")
+
+        for (i in 1..6) {
+            var cardBinding = CardMenuBinding.inflate(LayoutInflater.from(context))
+            cardBinding.txtName
+        }
+
+        Glide.with(this)
+            .asBitmap()
+            .load("https://www.11zon.com/images/home_icons/home_android_icon.png")
+            .into(object : CustomTarget<Bitmap>(){
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    //binding..setImageBitmap(resource)
+                }
+                override fun onLoadCleared(placeholder: Drawable?) {
+                    // this is called when imageView is cleared on lifecycle call or for
+                    // some other reason.
+                    // if you are referencing the bitmap somewhere else too other than this imageView
+                    // clear it here as you can no longer have the bitmap
+                }
+            })
 
         return binding.root
     }
