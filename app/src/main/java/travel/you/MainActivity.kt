7074bef14e05467ba1cee47e00323b27bceb58c6
Nikita Koschin <nik.koshin@mail.ru>
2021-10-23 09:39:41 +0300

    main layout creating

--- app/src/main/java/travel/you/MainActivity.kt
+++ app/src/main/java/travel/you/MainActivity.kt
@@ -2,10 +2,12 @@ package travel.you
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
--- app/src/main/java/travel/you/menu/MenuFragment.kt
+++ app/src/main/java/travel/you/menu/MenuFragment.kt
@@ -1,25 +1,17 @@
 package travel.you.menu
 
-import android.R
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 import travel.you.databinding.FragmentMenuBinding
-import android.widget.Toast
-
-import androidx.core.view.MenuItemCompat
-
-import androidx.core.content.ContextCompat.getSystemService
-
-import android.app.SearchManager
-
-import android.R.menu
 
 import android.view.MenuInflater
-import androidx.core.content.ContextCompat
-
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.appcompat.widget.Toolbar
+import travel.you.R
 
 class MenuFragment : Fragment() {
 
@@ -31,44 +23,10 @@ class MenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMenuBinding.inflate(inflater)
-        val toolbar = binding.toolbar
-        (activity as AppCompatActivity)
-            .setSupportActionBar(toolbar)
-        toolbar.setTitle("")
+
 
         return binding.root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.chat_screen_menu, menu)
-
-        val searchManager = getSystemService<Any>(Context.SEARCH_SERVICE) as SearchManager?
-        mSearchView =
-            MenuItemCompat.getActionView(menu.findItem(R.id.action_menu_search)) as SearchView
-        val toast = Toast(mApp)
-
-        if (mSearchView != null) {
-            mSearchView.setSearchableInfo(searchManager!!.getSearchableInfo(getComponentName()))
-            mSearchView.setIconifiedByDefault(false)
-            val queryTextListener: SearchView.OnQueryTextListener = object : OnQueryTextListener() {
-                fun onQueryTextChange(newText: String): Boolean {
-                    mSearchString = newText
-                    //doFilterAsync(mSearchString);
-                    toast.makeText(getApplicationContext(), "Test1", Toast.LENGTH_LONG).show()
-                    return true
-                }
-
-                fun onQueryTextSubmit(query: String): Boolean {
-                    mSearchString = query
-                    //doFilterAsync(mSearchString);
-                    toast.makeText(getApplicationContext(), "Test2", Toast.LENGTH_LONG).show()
-                    return true
-                }
-            }
-            mSearchView.setOnQueryTextListener(queryTextListener)
-        }
-
-        return true
-    }
 
 }
