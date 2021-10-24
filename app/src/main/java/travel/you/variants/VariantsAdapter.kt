4e5817d1bdd704020690acc945417630564160dd
1_skat_1 <skat.apps@gmail.com>
2021-10-24 09:59:34 +0300

    merge

---
+++ app/src/main/java/travel/you/variants/VariantsAdapter.kt
@@ -0,0 +1,30 @@
+package travel.you.variants
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import travel.you.R
+
+/*
+class VariantsAdapter(val variants: List<Varinat>) : RecyclerView.Adapter<VariantsAdapter.VariantsViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VariantsViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.text_row_item, viewGroup, false)
+
+        return
+    }
+
+    override fun onBindViewHolder(holder: VariantsViewHolder, position: Int) {
+        holder.bind()
+    }
+
+    override fun getItemCount(): Int {
+        return variants.size()
+    }
+
+    class VariantsViewHolder(view) :
+        RecyclerView.ViewHolder() {
+    }
+
+}*/
